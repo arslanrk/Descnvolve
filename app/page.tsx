debb71cfe74efe017a4574fbb41f2852ac2fe8df
arslangenex <arslan.rasheed@css-dev.com>
2024-08-27 15:28:21 +0500

    changes: project setup

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Banner from "./components/Banner";
+import Navbar from "./components/Navbar";
+
+export default function Home() {
+  return (
+    <main>
+      <Banner />
+    </main>
+  );
+}
